1146bafa91df3d8f06abeee3143c447fc91e2b97
Tomi Pieviläinen <tomi.pievilainen@iki.fi>
2015-02-16 14:00:14 +0200

    Remove unused code

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import os
 
 poikkeusURL = 'http://www.poikkeusinfo.fi/xml/v2/'
 
-gtfs_version = '20120224'
 agency_id = 'HSL'
 
 linetypes = {}
@@ -22,11 +21,6 @@ linetypes['12'] = 2 # lahiliikennejunat
 linetypes['36'] = 3 # Kirkkonummen sisainen
 linetypes['39'] = 3 # Keravan sisainen
 
-weekdays = ['Su', 'Ma', 'Ti', 'Ke', 'To', 'Pe', 'La', 'Su']
-weekdays[-1] = 'Su';
-lt = time.localtime()
-wd = weekdays[lt[6]+1]
-yd = weekdays[lt[6]]
 
 et = ElementTree
 tree = et.ElementTree()
@@ -45,7 +39,6 @@ def getDisruptions(lang):
   msg.header.gtfs_realtime_version = "1.0"
   msg.header.incrementality = msg.header.FULL_DATASET
   disruptions = tree.getroot()
-  # et.dump(disruptions)
   if (disruptions is not None):
     dtime = time.strptime(disruptions.attrib['time'], '%Y-%m-%dT%H:%M:%S')
     msg.header.timestamp = int(time.mktime(dtime))
@@ -83,7 +76,6 @@ def getDisruptions(lang):
           head.language = t.attrib['lang']
           head.text = t.text
 
-  # print msg
   return msg.SerializeToString()
 
 if __name__ == '__main__':
